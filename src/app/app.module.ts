85aedaa71a1380b55c668a95624aab34f18a07e1
Paul Rojas Pardo <paulepard@gmail.com>
2020-07-04 11:09:52 -0500

    dfdf
    sfdfdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 
 // Rutas
 import { APP_ROUTES } from './app.routes';
@@ -7,11 +7,15 @@ import { APP_ROUTES } from './app.routes';
 //Modulos
 import { PagesModule } from './pages/pages.module';
 
+//Temporal
+import { NgModule } from '@angular/core';
+
 //Componentes
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -23,7 +27,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     PagesModule,
-    APP_ROUTES
+    APP_ROUTES,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+
 import { PagesComponent } from './pages.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
 
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 @NgModule({
     declarations: [
@@ -14,6 +19,7 @@ import { PAGES_ROUTES } from './pages.routes';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent
     ],
     exports: [
         PagesComponent,
@@ -21,11 +27,12 @@ import { PAGES_ROUTES } from './pages.routes';
         ProgressComponent,
         Graficas1Component,
     ],
-    imports:[
+    imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
-export class PagesModule{}
+export class PagesModule{ }
 
